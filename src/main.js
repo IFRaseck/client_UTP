ad25e6efbafdec3c9e22ad172dfa4f85fa7f34b2
IFRaseck <leonhurtado22@gmail.com>
2021-10-12 16:37:07 -0500

    changes rutes

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ Vue.use(BootstrapVueIcons)
 Vue.use(VueAxios, axios)
 Vue.use(VueRouter)
 
-axios.defaults.baseURL = 'https://servidor-grupo1.herokuapp.com/';
+axios.defaults.baseURL = 'https://servidor-utp.herokuapp.com/';
 
 Vue.config.productionTip = false
 
